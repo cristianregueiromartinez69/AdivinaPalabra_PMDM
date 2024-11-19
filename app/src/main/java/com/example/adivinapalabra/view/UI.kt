0e34c46f558c3d90074fbe69a75dea1e417eaaec
cristian <cregueiromartinez@danielcastelao.org>
2024-11-19 16:55:38 +0100

    texto escribir palabra

--- app/src/main/java/com/example/adivinapalabra/view/UI.kt
+++ app/src/main/java/com/example/adivinapalabra/view/UI.kt
@@ -8,7 +8,13 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -22,7 +28,7 @@ import com.example.adivinapalabra.R
 
 @Composable
 fun MyApp(viewModel: ViewModel) {
-
+    val text by remember { mutableStateOf("") }
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -40,6 +46,9 @@ fun MyApp(viewModel: ViewModel) {
             }
             Row {
                 ShowSinonimo("hola")
+            }
+            Row {
+                TextNombreEscribir(remember { mutableStateOf(text) })
             }
         }
     }
@@ -79,3 +88,22 @@ fun ShowSinonimo(sinonimo:String){
 
 }
 
+@Composable
+fun TextNombreEscribir(text: MutableState<String>) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .padding(top = 20.dp, start = 16.dp)
+    ) {
+    }
+    TextField(
+        value = text.value,
+        onValueChange = { newText ->
+            text.value = newText
+        },
+        placeholder = { Text("palabra aqui...") },
+        modifier = Modifier
+            .padding(top = 150.dp, start = 16.dp)
+    )
+}
