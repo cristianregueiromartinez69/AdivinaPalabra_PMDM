70c7fbb4efefab672c32e1ee8777c6c6fd4c5a57
cristian <cregueiromartinez@danielcastelao.org>
2024-11-19 17:30:03 +0100

    getters de datos

--- app/src/main/java/com/example/adivinapalabra/viewmodel/ViewModel.kt
+++ app/src/main/java/com/example/adivinapalabra/viewmodel/ViewModel.kt
@@ -2,6 +2,7 @@ package com.example.adivinapalabra.viewmodel
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.example.adivinapalabra.model.Datos
 import com.example.adivinapalabra.model.Diccionario
 import kotlin.random.Random
 
@@ -19,9 +20,33 @@ class ViewModel:ViewModel() {
     private fun checkPalabra(id: Int): String {
         val palabra = Diccionario.entries.find { it.id == id }
         if (palabra != null) {
+            Datos.palabra = palabra.nombre
             return palabra.nombre
         }
         return ""
     }
+
+    fun getPalabra():String{
+        return Datos.palabra
+    }
+
+    fun getSinonimo():String{
+        return Datos.sinonimo
+    }
+
+    fun getRonda():Int{
+        return Datos.ronda
+    }
+
+    fun getAciertos():Int{
+        return Datos.aciertos
+    }
+
+    fun getFallos():Int{
+        return Datos.fallos
+    }
+
+
+
 
 }
