c07c0c88e57d1856ed1c5ba44d1a51fbd3320add
cristian <cregueiromartinez@danielcastelao.org>
2024-11-19 17:21:04 +0100

    metodo nombre hecho

--- app/src/main/java/com/example/adivinapalabra/viewmodel/ViewModel.kt
+++ app/src/main/java/com/example/adivinapalabra/viewmodel/ViewModel.kt
@@ -1,6 +1,23 @@
 package com.example.adivinapalabra.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.adivinapalabra.model.Diccionario
+import kotlin.random.Random
 
 class ViewModel:ViewModel() {
+
+    val random = Random
+    fun setPalabraDir(){
+        var numeroAleatorioDiccionario = random.nextInt(1,21)
+        var palabraDir = checkPalabra(numeroAleatorioDiccionario)
+    }
+
+    fun checkPalabra(id: Int): String {
+        val palabra = Diccionario.entries.find { it.id == id }
+        if (palabra != null) {
+            return palabra.nombre
+        }
+        return ""
+    }
+
 }
