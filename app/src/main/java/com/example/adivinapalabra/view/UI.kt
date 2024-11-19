68b6ad47f99ea2777995b9c6aa00b751ff145e0e
cristian <cregueiromartinez@danielcastelao.org>
2024-11-19 16:41:48 +0100

    rondas hecho

--- app/src/main/java/com/example/adivinapalabra/view/UI.kt
+++ app/src/main/java/com/example/adivinapalabra/view/UI.kt
@@ -1,12 +1,22 @@
 package com.example.adivinapalabra.view
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
 import com.example.adivinapalabra.R
 
@@ -24,5 +34,28 @@ fun MyApp(viewModel: ViewModel) {
             contentScale = ContentScale.FillHeight,
             modifier = Modifier.fillMaxSize()
         )
+        Column {
+            Row {
+                ShowRondas(0)
+            }
+        }
     }
 }
+
+@Composable
+fun ShowRondas(rondas:Int){
+    Column(verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .padding(top = 30.dp, start = 115.dp)
+    ) {
+
+        Text(text = "Rondas: $rondas" ,
+            fontSize = 25.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White)
+
+    }
+
+}
+
